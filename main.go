e02aaf8406a2d5c4ff0b4de623fe33413c46c507
PedroAntonioKira <morales.gonzalez.pedroantonio@gmail.com>
2024-05-04 19:05:48 -0600

    Actualizacion de archivos a Repositorio

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/PedroAntonioKira/ecommerceEscom/variables"
+import (
+	"github.com/PedroAntonioKira/ecommerceEscom/funciones"
+	"github.com/PedroAntonioKira/ecommerceEscom/variables"
+)
 
 func main() {
 	variables.MostrarEnteros()
+
+	funciones.RestoVariables()
 }
